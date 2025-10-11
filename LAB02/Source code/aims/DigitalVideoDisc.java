3a8243b71f59aab72be8d0032a23fdc6edb0d100
NguyenKhacHoangPhuc <tenemailcuaem@gmail.com>
2025-10-11 17:19:52 +0700

    add lab02

---
+++ LAB02/Source code/aims/DigitalVideoDisc.java
@@ -0,0 +1,48 @@
+package aims;
+
+public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String directory;
+	private int length;
+	private float cost;
+	public String getTitle() {
+		return title;
+	}
+	public DigitalVideoDisc(String title) {
+		this.title =title;
+	}
+	public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.directory = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.directory = director;
+        this.length = length;
+        this.cost = cost;
+    }
+	
+	public String getCategory() {
+		return category;
+	}
+	public String getDirectory() {
+		return directory;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+	
+}
+ 
