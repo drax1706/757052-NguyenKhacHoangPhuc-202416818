efc410ce2001a4e161d79ceb24ed125a808a37be
NguyenKhacHoangPhuc <tenemailcuaem@gmail.com>
2025-10-26 15:54:13 +0700

    upload topic class

--- LAB02/Source code/aim/DigitalVideoDisc.java
+++ LAB02/Source code/aim/DigitalVideoDisc.java
@@ -6,22 +6,34 @@ public class DigitalVideoDisc {
 	private String directory;
 	private int length;
 	private float cost;
+	private int id;
+   
+	
+    private static int nbDigitalVideoDiscs = 0;
+	
+	
 	public String getTitle() {
 		return title;
 	}
 	public DigitalVideoDisc(String title) {
 		this.title =title;
+		nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String category, String title, float cost) {
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String director, String category, String title, float cost) {
         this.directory = director;
         this.category = category;
         this.title = title;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
         this.title = title;
@@ -29,8 +41,10 @@ public class DigitalVideoDisc {
         this.directory = director;
         this.length = length;
         this.cost = cost;
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
     }
-
+    
 	
 	public String getCategory() {
 		return category;
@@ -44,6 +58,12 @@ public class DigitalVideoDisc {
 	public float getCost() {
 		return cost;
 	}
+	public int getId() {
+        return id;
+    }
+    public static int getNbDigitalVideoDiscs() {
+        return nbDigitalVideoDiscs;
+    }
 	
     public void setTitle(String title) {
     	this.title =title ; 
